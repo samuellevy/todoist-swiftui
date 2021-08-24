e4dee45c06f15220ad5158905c1a0a1263637a39
Samuel Levy <samuellevy.dev@gmail.com>
2021-08-24 17:03:56 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Samuel Levy on 24/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
